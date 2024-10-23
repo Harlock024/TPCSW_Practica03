d64a881c0923e015f963ebdbd18e922ded561e4a
harlock024 <hadith024@gmail.com>
2024-10-22 19:29:30 -0600

    initial commit practica 3

--- src/main/java/org/uv/tpcsw/practica03/TPCSWPractica03.java
+++ src/main/java/org/uv/tpcsw/practica03/TPCSWPractica03.java
@@ -1,46 +1,64 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package org.uv.tpcsw.practica03;
 
+import java.util.List;
+import java.util.Set;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
-/**SessionFactory
+/**
  *
  * @author harlock024
+ *
  */
 public class TPCSWPractica03 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-       
-        Departamento departamento= new Departamento();
-        departamento.setNombre("administracion");
 
-//        Empleado emp= new Empleado();
-//        emp.setClave(02);
-//        emp.setNombre("hadith hirbenate");
-//        emp.setDireccion("av 2");
-//        emp.setTelefono("27211234245");
-//        
-        SessionFactory sf= HibernateUtil.getSessionFactory();
-         Session session = sf.getCurrentSession();
-        Transaction t= session.beginTransaction();
+        Empleado emp2 = new Empleado();
+        emp2.setClave(05);
+        emp2.setNombre("Victor hirbenate");
+        emp2.setDireccion("av 6");
+        emp2.setTelefono("27215455");
+
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session s = sf.getCurrentSession();
+        Transaction t = s.beginTransaction();
+
+        Departamento depto = s.get(Departamento.class, 1L);
+        if (depto != null) {
+            System.out.println("Clave:" + depto.getClave() + "Nombre:" + depto.getNombre());
+            Set<Empleado> empleados = depto.getEmpleados();
+
+            for (Empleado empleado : empleados) {
+                System.out.println("clave:" + empleado.getNombre());
+            }
+        }
         
         
-       Departamento dep = session.get(Departamento.class,1L);
-       
-       
-     if (dep==null){
-         
-     }
-                
-                
-        session.save(departamento);
+        Empleado empleadoExiste = s.get(Empleado.class, emp2.getClass());
+        if(emp2!=null){
+            emp2.setNombre("hadith ");
+            s.update(empleadoExiste);
+            System.out.println("Empleado update: "+ empleadoExiste.getNombre());
+         }
+        
+        
+        if(empleadoExiste!=null){
+            s.delete(empleadoExiste);
+            System.out.println("Empleado delete: "+ emp2.getNombre());
+        }
+        
+        List<Empleado> ListE = s.createQuery("from Empleado").list();
+        System.out.println("Empleados");
+        for (Empleado emp : ListE) {
+            System.out.println("Nombre:" +emp.getNombre());
+           
+        }
+        
         t.commit();
+        
+
     }
 }
-
